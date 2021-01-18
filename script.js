e97e2461ae397d86cdf18fb312ad0e55d79cd137
Saadia El fekak <saadiaelfekak@gmail.com>
2021-01-18 15:00:39 +0000

    Adding a timer function and an event listener to the start button

---
+++ script.js
@@ -0,0 +1,21 @@
+var timeEl = document.getElementById("timer");
+var startBtn = document.getElementById("startButton");
+var secondsLeft = 60;
+
+function startTimer() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time : " + secondsLeft;
+
+    }, 1000);
+
+    if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+    };
+};
+
+startBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    startTimer()
+
+});
