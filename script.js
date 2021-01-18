0e8991cf80b8085ca45519b8436caa927bdd1c6c
Saadia El fekak <saadiaelfekak@gmail.com>
2021-01-18 15:42:47 +0000

    Updating questions object

--- script.js
+++ script.js
@@ -7,117 +7,50 @@ var Questions =
 {
     question: "What does HTML stand for ?",
     answers: {
-        a: {
-            q: "Hyper Text Markup Language",
-            value: true
-        },
-        b: {
-            q: "High Text Markup Language",
-            value: false
-        },
-        c: {
-            q: "Hyper Tabular Markup Language",
-            value: false
-        },
-        d: {
-            q: "None of these",
-            value: false
-        },
+        a: ["Hyper Text Markup Language", true],
+        b: ["High Text Markup Language", false],
+        c: ["Hyper Tabular Markup Language", false],
+        d: ["None of these", false]
     },
+
     question: "Markup tags tell the web browser :",
     answers: {
-        a: {
-            q: "How to organise the page",
-            value: false
-        },
-        b: {
-            q: "How to display the page",
-            value: true
-        },
-        c: {
-            q: "How to display message box on page",
-            value: false
-        },
-        d: {
-            q: "None of these",
-            value: false
-        },
+        a: ["How to organise the page", false],
+        b: ["How to display the page", true],
+        c: ["How to display message box on page", false],
+        d: ["None of these", false],
     },
+
     question: "Web pages start with which of the following tag ?",
     answers: {
-        a: {
-            q: "<body>",
-            value: false
-        },
-        b: {
-            q: "<head>",
-            value: false
-        },
-        c: {
-            q: "html",
-            value: true
-        },
-        d: {
-            q: "<title>",
-            value: false
-        },
+        a: ["<body>", false],
+        b: ["<head>", false],
+        c: ["html", true],
+        d: ["<title>", false],
     },
+
     question: "How can you open a link in a new browser window ?",
     answers: {
-        a: {
-            q: "<a href = url target = new>",
-            value: false
-        },
-        b: {
-            q: "<a href = url target = _blank>",
-            value: true
-        },
-        c: {
-            q: "<a href = url.new>",
-            value: false
-        },
-        d: {
-            q: "<a href = url target = open>",
-            value: false
-        },
+        a: ["<a href = url target = new>", false],
+        b: ["<a href = url target = _blank>", true],
+        c: ["<a href = url.new>", false],
+        d: ["<a href = url target = open>", false],
     },
+
     question: "What tag is used to display a picture in a HTML page ?",
     answers: {
-        a: {
-            q: "<picture>",
-            value: false
-        },
-        b: {
-            q: "<image>",
-            value: false
-        },
-        c: {
-            q: "<img>",
-            value: true
-        },
-        d: {
-            q: "<src>",
-            value: false
-        },
+        a: ["<picture>", false],
+        b: ["<image>", false],
+        c: ["<img>", true],
+        d: ["<src>", false],
     },
+
     question: "HTML web pages can be read and rendered by _________.",
     answers: {
-        a: {
-            q: "Compiler",
-            value: false
-        },
-        b: {
-            q: "Server",
-            value: false
-        },
-        c: {
-            q: "Interpreter",
-            value: false
-        },
-        d: {
-            q: "Browser",
-            value: true
-        },
+        a: ["Compiler", false],
+        b: ["Server", false],
+        c: ["Interpreter", false],
+        d: ["Browser", true],
     },
 };
 
@@ -125,7 +58,6 @@ function startTimer() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = "Time : " + secondsLeft;
-
     }, 1000);
 
     if (secondsLeft === 0) {
