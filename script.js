f379d9fff7dc1e4b1b627d8d0b28f7c017718800
Saadia El fekak <saadiaelfekak@gmail.com>
2021-01-24 21:26:07 +0000

    Updating questions and adding createQuestions function

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
 var timeEl = document.getElementById("timer");
 var startBtn = document.getElementById("startButton");
 var startPage = document.getElementById("startPage");
-var question = document.getElementById("question");
+var questionContainer = document.getElementById("question");
+var questionText = document.getElementById("questionText");
+var row = document.getElementById("row");
 var secondsLeft = 60;
-var Questions =
-{
-    question: "What does HTML stand for ?",
-    answers: {
-        a: ["Hyper Text Markup Language", true],
-        b: ["High Text Markup Language", false],
-        c: ["Hyper Tabular Markup Language", false],
-        d: ["None of these", false]
+var Questions = [
+    {
+        question: "What does HTML stand for ?",
+        answers: [
+            ["Hyper Text Markup Language", true],
+            ["High Text Markup Language", false],
+            ["Hyper Tabular Markup Language", false],
+            ["None of these", false]
+        ]
     },
 
-    question: "Markup tags tell the web browser :",
-    answers: {
-        a: ["How to organise the page", false],
-        b: ["How to display the page", true],
-        c: ["How to display message box on page", false],
-        d: ["None of these", false],
+    {
+        question: "Markup tags tell the web browser :",
+        answers: [
+            ["How to organise the page", false],
+            ["How to display the page", true],
+            ["How to display message box on page", false],
+            ["None of these", false],
+        ]
     },
 
-    question: "Web pages start with which of the following tag ?",
-    answers: {
-        a: ["<body>", false],
-        b: ["<head>", false],
-        c: ["html", true],
-        d: ["<title>", false],
+    {
+        question: "Web pages start with which of the following tag ?",
+        answers: [
+            ["<body>", false],
+            ["<head>", false],
+            ["html", true],
+            ["<title>", false],
+        ]
     },
 
-    question: "How can you open a link in a new browser window ?",
-    answers: {
-        a: ["<a href = url target = new>", false],
-        b: ["<a href = url target = _blank>", true],
-        c: ["<a href = url.new>", false],
-        d: ["<a href = url target = open>", false],
+    {
+        question: "How can you open a link in a new browser window ?",
+        answers: [
+            ["<a href = url target = new>", false],
+            ["<a href = url target = _blank>", true],
+            ["<a href = url.new>", false],
+            ["<a href = url target = open>", false],
+        ]
     },
 
-    question: "What tag is used to display a picture in a HTML page ?",
-    answers: {
-        a: ["<picture>", false],
-        b: ["<image>", false],
-        c: ["<img>", true],
-        d: ["<src>", false],
+    {
+        question: "What tag is used to display a picture in a HTML page ?",
+        answers: [
+            ["<picture>", false],
+            ["<image>", false],
+            ["<img>", true],
+            ["<src>", false],
+        ]
     },
 
-    question: "HTML web pages can be read and rendered by _________.",
-    answers: {
-        a: ["Compiler", false],
-        b: ["Server", false],
-        c: ["Interpreter", false],
-        d: ["Browser", true],
+    {
+        question: "HTML web pages can be read and rendered by _________.",
+        answers: [
+            ["Compiler", false],
+            ["Server", false],
+            ["Interpreter", false],
+            ["Browser", true],
+        ]
     },
-};
+];
 
 function startTimer() {
     var timerInterval = setInterval(function () {
@@ -69,6 +82,48 @@ startBtn.addEventListener("click", function (event) {
     event.preventDefault();
     startTimer();
     startPage.style.display = "none";
-    question.style.display = "block";
+    questionContainer.style.display = "block";
+    nextQuestion(0);
+   
 });
 
+function createQuestions(i) {
+
+    var h2Tag = document.createElement("H2");
+    h2Tag.innerHTML = Questions[i].question;
+    var col = document.createElement("DIV");
+    col.setAttribute("id", "question0" + (i + 1));
+    col.setAttribute("class", "col-6");
+    col.setAttribute("style", "margin-top : 30px");
+    col.appendChild(h2Tag);
+    //document.getElementById("question0"+ (i + 1)).style.display = "block";
+    //col.style.display = "block";
+    Questions[i].answers.forEach(function (item, j) {
+
+        var btn = document.createElement("BUTTON");
+        btn.setAttribute("id", "choice0" + (j + 1));
+        btn.setAttribute("class", "btn btn-outline-info btn-lg btn-rounded btn-block");
+        btn.textContent = Questions[i].answers[j][0];
+        row.appendChild(col);
+        col.appendChild(btn);
+    });
+
+};
+
+function clickBtn(a) {
+    var btns = document.querySelectorAll('.btn')
+    btns.forEach(function (btn, i) {
+        btn.addEventListener("click", function (event) {
+            event.preventDefault();
+            document.getElementById("question0"+a).style.display = "none";
+            createQuestions(a);
+        });
+    });
+};
+
+
+function nextQuestion(a) {
+    createQuestions(a);
+    clickBtn(a+1);
+    
+}
